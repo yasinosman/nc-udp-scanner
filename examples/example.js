86a6480ae94ec1a09297a420db52ed66aa01da11
Yasin Osman <yasinosman10@gmail.com>
2021-01-17 08:23:47 +0300

    documentation

---
+++ examples/example.js
@@ -0,0 +1,24 @@
+const { udpScan } = require("./lib/scanner");
+
+function scan(ip, range) {
+    const hrstart = process.hrtime();
+    udpScan(ip, range)
+        .then((report) => {
+            printParsedUdpScanReport(report);
+            const hrend = process.hrtime(hrstart);
+            console.info('Executed in: %ds %dms', hrend[0], hrend[1] / 1000000)
+            scan(ip, range);
+        })
+        .catch((error) => console.log(error));
+}
+
+const IP = "192.168.1.4";
+const portRange = "5061-5074";
+scan(IP, portRange);
+
+const printParsedUdpScanReport = (report) => {
+    console.log({
+        Header: "PortScan",
+        Status: report,
+    });
+};
